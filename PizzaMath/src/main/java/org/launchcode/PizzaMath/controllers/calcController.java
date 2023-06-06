1c57a515d518320c9e34f48eeb133722e432ff3e
Amy Stockhausen <stockhausenamy@gmail.com>
2023-06-05 21:38:10 -0500

    Continued calculation work.

--- PizzaMath/src/main/java/org/launchcode/PizzaMath/controllers/calcController.java
+++ PizzaMath/src/main/java/org/launchcode/PizzaMath/controllers/calcController.java
@@ -23,13 +23,31 @@ public class calcController {
         double mediumArea = Math.pow(customCalculation.getMediumSize()/2, 2) * 3.14;
         double largeArea = Math.pow(customCalculation.getLargeSize()/2, 2) * 3.14;
         double xLargeArea = Math.pow(customCalculation.getxLargeSize()/2, 2) * 3.14;
+        double[] pizzaSizeArr = {smallArea, mediumArea, largeArea, xLargeArea};
+        double[] pizzaCountArr = {0, 0, 0, 0};
 
-        double totalPizzaArea = (customCalculation.getSmallCount() * 4.5) +
-                (customCalculation.getMediumCount() * 9) +
-                (customCalculation.getTeenCount() * 18) +
-                (customCalculation.getAdultCount() * 13.5);
+        double totalPizzaArea = (customCalculation.getSmallCount() * 14.13) +
+                (customCalculation.getMediumCount() * 28.26) +
+                (customCalculation.getTeenCount() * 56.52) +
+                (customCalculation.getAdultCount() * 42.39);
 
         double minSliceCount = customCalculation.getSmallCount() + customCalculation.getMediumCount() + customCalculation.getTeenCount() * 2 + customCalculation.getAdultCount() * 2;
+        double minNumberPizzas = minNumberPizzas(minSliceCount);
+
+        for (int i = 0; i < 3; i++) {
+            while (totalPizzaArea/minNumberPizzas >= pizzaSizeArr[i] && totalPizzaArea/minNumberPizzas < pizzaSizeArr[i+1]) {
+                pizzaCountArr[i]++;
+                totalPizzaArea = totalPizzaArea - pizzaSizeArr[i];
+                minNumberPizzas--;
+            }
+        }
+        if (minSliceCount - (minNumberPizzas * 8) > 0) {
+            for (int k = 0; k < 3; k++) {
+                if (totalPizzaArea <= pizzaSizeArr[k]) {
+                    pizzaCountArr[k]++;
+                }
+            }
+        }
 
         return "result";
     }
@@ -37,7 +55,7 @@ public class calcController {
     private double minNumberPizzas (double minSliceCount) {
         double numberPizzaRec = 0;
         for (int i = 1; i < 1000; i++) {
-            if (minSliceCount - (i * 8) >= 4) {
+            if (minSliceCount - (i * 8) >= 0) {
                 numberPizzaRec++;
             }
         }
